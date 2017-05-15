f95149e47e016df309385fab424812aa5cf300c9
smendez92 <samuelremndez14@gmail.com>
2017-05-14 23:55:30 -0500

    basic functions of game except for win or loss check and random target word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,18 @@
-//funtion returns all indices of given letter in a given string. Undefined if letter is not in string.
-function checkLetterInWord(letter, word){
+//funtion loops through word and returns each index that the letter occupies. Returns a value of -1 for letterNumber if letter is not in word.
+function checkLetterInWord(letter, word, wordArray){
+	var result = {
+		wordArray: [],
+		letterNumber: -1,
+	};
+
 	for (var letterNumber = word.indexOf(letter); letterNumber >= 0; letterNumber = word.indexOf(letter, letterNumber + 1)){
-    	return (letterNumber);
-	}
+    		wordArray[letterNumber] = letter;
+    		result.wordArray = wordArray;
+    		result.letterNumber = letterNumber;
+    	}
+    return result;
 }
+
 
 function pushAndPrintString(string1, array1, htmlElementVariable){		
 	//Push the latest guess to the guessedLettersArray
